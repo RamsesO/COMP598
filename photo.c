6b2232a63bc95883699685c4032b6d06d88d55ed
Unknown <ordonez.ramses@gmail.com>
2018-02-23 13:22:06 -0800

    photo

---
+++ photo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <ncurses.h>
+
+#include <ev3.h>
+#include <ev3_port.h>
+#include <ev3_sensor.h>
+
+#include <unistd.h>
+#define Sleep(msec) usleep((msec) * 1000)
+
+int main(int argc, char** argv)
+{
+	char s[256];
+	int val;
+	uint32_t n, i, ii;
+	uint8_t sn_color;
+
+	if(ev3_init() < 1) return 1;
+
+	for(i = 0; i < DESC_LIMIT; i++)
+	{
+		if ( ev3_sensor[ i ].type_inx != SENSOR_TYPE__NONE_ ) 
+		{
+            printf( "  type = %s\n", ev3_sensor_type( ev3_sensor[ i ].type_inx ));
+            printf( "  port = %s\n", ev3_sensor_port_name( i, s ));
+            if ( get_sensor_mode( i, s, sizeof( s ))) 
+            {
+                printf( "  mode = %s\n", s );
+            }
+            if ( get_sensor_num_values( i, &n )) 
+            {
+                for ( ii = 0; ii < n; ii++ ) 
+                {
+                    if ( get_sensor_value( ii, i, &val )) 
+                    {
+                        printf( "  value%d = %d\n", ii, val );
+                    }
+                }
+            }
+        }
+	}
+
+	if ( ev3_search_sensor( LEGO_EV3_COLOR, &sn_color, 0 )) {
+        printf( "COLOR sensor is found, reading COLOR...\n" );
+        set_sensor_mode( sn_color, "COL-COLOR" );
+        for ( ; ; ) {
+            if ( !get_sensor_value( 0, sn_color, &val ) || ( val < 0 ) || ( val >= COLOR_COUNT )) {
+                val = 0;
+            }
+            printf( "\r(%s)", color[ val ]);
+            fflush( stdout );
+            if ( _check_pressed( sn_touch )) break;
+            Sleep( 200 );
+            printf( "\r        " );
+            fflush( stdout );
+            if ( _check_pressed( sn_touch )) break;
+            Sleep( 200 );
+        }
+    } else {
+        printf( "COLOR sensor is NOT found\n" );
+        while ( !_check_pressed( sn_touch )) Sleep( 100 );
+    }
+
+    ev3_uninit();
+
+}
