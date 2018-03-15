20921ef816a684272e83c8cce5e41cb9f0c4c48e
Daniel Benusovich <dbenusovich@gmail.com>
2018-03-14 21:20:41 -0700

    Added multi touch (maybe) got rid of macros

--- l.c
+++ l.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "coroutine.h"
 #include "ev3.h"
 #include "ev3_port.h"
 #include "ev3_sensor.h"
@@ -16,15 +15,20 @@
 #define Sleep( msec ) usleep(( msec ) * 1000 )
 #endif
 
-#define L_MOTOR_PORT      OUTPUT_A
-#define L_MOTOR_EXT_PORT  EV3_PORT__NONE_
-#define R_MOTOR_PORT      OUTPUT_B
-#define R_MOTOR_EXT_PORT  EV3_PORT__NONE_
-#define SPEED_LINEAR      75
-#define SPEED_CIRCULAR    50
-#define CIRCLE_RADIUS     203.2 // metric milimeters
-#define TIRE_RADIUS       21.6 // diameter is 43.2
-#define MAX_BLACK_RANGE   15
+#define L_MOTOR_PORT      			OUTPUT_A
+#define L_MOTOR_EXT_PORT  			EV3_PORT__NONE_
+#define R_MOTOR_PORT      			OUTPUT_B
+#define R_MOTOR_EXT_PORT  			EV3_PORT__NONE_
+//I do not know if this will work!
+#define L_TOUCH_SENSOR_PORT	  		OUTPUT_1 //<- is this a valid reference?
+#define L_TOUCH_SENSOR_EXT_PORT  	EV3_PORT__NONE_
+#define L_TOUCH_SENSOR_PORT   		OUTPUT_2
+#define R_TOUCH_SENSOR_EXT_PORT   	EV3_PORT__NONE_
+#define SPEED_LINEAR      			75
+#define SPEED_CIRCULAR    			50
+#define CIRCLE_RADIUS     			203.2 // metric millimeters
+#define TIRE_RADIUS       			21.6 // diameter is 43.2
+#define MAX_BLACK_RANGE   			15
 
 int max_speed;  /* Motor maximal speed */
 int is_button_pressed = 0;
@@ -51,6 +55,7 @@ uint8_t sn_touch, sn_color;
 // to use multi set you need array of motors followed by set of desc limit
 enum{ L,R}; // use this to set indicies of motor
 uint8_t motor[ 3 ] = {DESC_LIMIT,DESC_LIMIT,DESC_LIMIT};
+uint8_t touch_sensor[3] = {DESC_LIMIT,DESC_LIMIT,DESC_LIMIT};
 
 static bool withinColRange(int beg, int end)
 {
@@ -115,7 +120,7 @@ int turn_vehicle(int motor_speed, int number_of_rotations){
     multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
 
     printf("I am in here\n");
-    //Get the inital reference for encoders
+    //Get the initial reference for encoders
     int initial_encoder_reading_L = encoder_count[L];
     int initial_encoder_reading_R = encoder_count[R];
     //If the user requests a rotation
@@ -127,19 +132,19 @@ int turn_vehicle(int motor_speed, int number_of_rotations){
             if(!get_sensor_value(0, sn_color, &color_value)){
                 color_value = 0;
             }
-            if(_check_pressed(sn_touch)){
+            if(_check_pressed(touch_sensor[L]) || _check_pressed(touch_sensor[R]){
                 is_button_pressed = 1;
             }
         }
     } else {
         //Turn right
-        //Loop until the couner counts are less than the inital reading
+        //Loop until the counter is less than the initial reading
         while(encoder_count[R] < initial_encoder_reading_R + (360 * number_of_rotations) && !is_button_pressed && !withinColRange(0,MAX_BLACK_RANGE)){
             get_tacho_position(motor[R], &encoder_count[R]);
              if(!get_sensor_value(0, sn_color, &color_value)){
                 color_value = 0;
             }
-            if(_check_pressed(sn_touch)){
+            if(_check_pressed(touch_sensor[L]) || _check_pressed(touch_sensor[R]){
                 is_button_pressed = 1;
             }
         }
@@ -194,6 +199,8 @@ static int _distance_to_degrees(double distance)
 int app_init( void )
 {
     char s[ 16 ];
+	
+	//Attempting to find left and right motors
     if ( ev3_search_tacho_plugged_in( L_MOTOR_PORT, L_MOTOR_EXT_PORT, motor + L, 0 )) {
         get_tacho_max_speed( motor[ L ], &max_speed );
         /* Reset the motor */
@@ -212,123 +219,28 @@ int app_init( void )
         return ( 0 );
     }
 
+	//Activating color sensor
     if ( ev3_search_sensor( LEGO_EV3_COLOR, &sn_color, 0 )) {
         printf("color activated");
     }
     command = moving = MOVE_NONE;
-    if ( ev3_search_sensor( LEGO_EV3_TOUCH, &sn_touch, 0 )) {
-        printf( " use the TOUCH sensor.\n" );
+	
+	//Attempting to find left and right touch sensors
+	//TODO verify these function calls exist and delete this comment.
+    if ( ev3_search_sensor_plugged_in(L_TOUCH_SENSOR_PORT, L_TOUCH_SENSOR_EXT_PORT, touch_sensor + L, 0 )) {
+        //printf( " use the TOUCH sensor.\n" );
     } else {
         sn_touch = DESC_LIMIT;
-        printf( " press UP on the EV3 brick.\n" );
+        printf( "LEFT touch sensor (%s) not found.\n", ev3_port_name( L_TOUCH_SENSOR_PORT, L_TOUCH_SENSOR_EXT_PORT, 0, s ));
+    }
+	if ( ev3_search_sensor_plugged_in(L_TOUCH_SENSOR_PORT, L_TOUCH_SENSOR_EXT_PORT, touch_sensor + L, 0 )) {
+        //printf( " use the TOUCH sensor.\n" );
+    } else {
+        sn_touch = DESC_LIMIT;
+        printf( "RIGHT touch sensor (%s) not found.\n", ev3_port_name( R_TOUCH_SENSOR_PORT, R_TOUCH_SENSOR_EXT_PORT, 0, s ));
     }
     return ( 1 );
 }
-
-/*
-CORO_CONTEXT( turn );
-
-
-
-*/
-CORO_CONTEXT( handle_touch );
-CORO_CONTEXT( handle_color);
-CORO_CONTEXT( drive );
-CORO_CONTEXT( measure_distance);
-
-
-CORO_DEFINE( handle_touch )
-{
-    CORO_LOCAL int val;
-    CORO_BEGIN();
-    if ( sn_touch == DESC_LIMIT ) CORO_QUIT();
-    for ( ; ; ) {
-        // Waiting the button is pressed
-        CORO_WAIT( get_sensor_value( 0, sn_touch, &val ) && ( val ));
-        // Stop the vehicle
-        if (command == MOVE_BACKWARD)
-        {
-            command = MOVE_FORWARD;
-        }else{
-            command = MOVE_BACKWARD;
-        }
-
-    }
-    CORO_END();
-}
-
-CORO_DEFINE( handle_color)
-{
-    CORO_LOCAL int color_val;
-    CORO_BEGIN();
-    set_sensor_mode( sn_color, "COL-REFLECT" );
-        for ( ; ; ) {
-
-            if ( !get_sensor_value( 0, sn_color, &color_val ) || ( color_val < 0 ) )
-            {
-                color_val = 0;
-            }
-            if (color_val == 0)
-            {
-                CORO_CALL(measure_distance);
-            }
-            CORO_YIELD();
-        }
-
-
-    CORO_END();
-}
-
-CORO_DEFINE(measure_distance)
-{
-    CORO_LOCAL int l_travled, r_travled;
-    CORO_BEGIN();
-    int l_pos,l_pos_end;
-    int r_pos,r_pos_end;
-    get_tacho_position(motor[L],&l_pos);
-    get_tacho_position(motor[R],&r_pos);
-    for( ; ; ){
-        CORO_WAIT(l_travled != 1);
-        get_tacho_position( motor[L], &l_pos_end);
-        get_tacho_position( motor[R], &r_pos_end);
-        l_travled = l_pos_end;
-        r_travled = r_pos_end;
-    }
-    CORO_END();
-}
-
-CORO_DEFINE( drive )
-{
-    CORO_LOCAL int speed_linear, speed_circular;
-    CORO_LOCAL int _wait_stopped;
-    CORO_BEGIN();
-    speed_linear = max_speed/2;
-    speed_circular = max_speed/2;
-    for ( ; ; ) {
-        // /* Waiting new command
-        CORO_WAIT( moving != command );
-        _wait_stopped = 0;
-        switch ( command ) {
-        case MOVE_NONE:
-            _stop();
-            _wait_stopped = 1;
-            break;
-        case MOVE_FORWARD:
-            _run_forever( speed_linear, speed_linear );
-            break;
-        case MOVE_BACKWARD:
-            _run_forever( -speed_linear, -speed_linear );
-            break;
-        case TURN:
-            turn_vehicle(speed_circular, 10);
-        }
-        moving = command;
-        if ( _wait_stopped ) {
-            // Waiting the command is completed
-            //CORO_WAIT( !_is_running());
-            command = moving = MOVE_NONE;
-        }
-    }
 }
 
 
@@ -345,46 +257,77 @@ int main( void )
     printf( "Waiting the EV3 brick online...\n" );
     if ( ev3_init() < 1 ) return ( 1 );
     printf( "*** ( EV3 ) Hello! ***\n" );
+	//Initialize sensors
     ev3_sensor_init();
+	//Initialize motors
     ev3_tacho_init();
+	//Assign all constants etc for operation
     app_alive=app_init();
     set_motor_variables();
-    //command = TURN;
     is_button_pressed = 0;
     srand(time(NULL));
-    while(app_alive){ //&& !withinColRange(0,MAX_BLACK_RANGE)){
-       set_tacho_speed_sp(motor[L], max_speed/2);
-       set_tacho_speed_sp(motor[R], max_speed/2);
-       multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
-       while(!is_button_pressed && !withinColRange(0,MAX_BLACK_RANGE)){
-            if(_check_pressed(sn_touch)){
+	
+	//Operational logic
+	//This loop will never terminate!
+    while(app_alive) {
+		//Set vehicle to move forward with half of the max speed for both sides
+        set_tacho_speed_sp(motor[L], max_speed/2);
+        set_tacho_speed_sp(motor[R], max_speed/2);
+        multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
+		
+		//drive forward until:
+		//A touch sensor is activated
+		//The color sensor detects a black object
+        while(!is_button_pressed && !withinColRange(0,MAX_BLACK_RANGE)){
+			//Check that either touch sensor has been activated
+            if(_check_pressed(touch_sensor[L]) || _check_pressed(touch_sensor[R]){
                 is_button_pressed = 1;
             }
+			//Get new color value
             get_sensor_value(0, sn_color, &color_value);
         }
-       if(!withinColRange(0,MAX_BLACK_RANGE)){
-           multi_set_tacho_command_inx(motor, TACHO_STOP);
-           set_tacho_speed_sp(motor[L], -max_speed/2);
-           set_tacho_speed_sp(motor[R], -max_speed/2);
-           multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
-           Sleep(1000);
-           multi_set_tacho_command_inx(motor, TACHO_STOP);
-           is_button_pressed = 0;
-           turn_vehicle(max_speed/2, rand()%6);
-       } else {
-           set_tacho_speed_sp(motor[L], max_speed);
-           set_tacho_speed_sp(motor[R], max_speed);
-           multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
-           Sleep(200);
-           multi_set_tacho_command_inx(motor, TACHO_STOP);
-
-       }
+		
+		//If the color currently detected is not within the black threshold
+		//activate the backup and random turning sequence
+        if(!withinColRange(0,MAX_BLACK_RANGE)){
+			//Order motors to stop
+            multi_set_tacho_command_inx(motor, TACHO_STOP);
+			//Set to go in reverse and drive backwards
+            set_tacho_speed_sp(motor[L], -max_speed/2);
+            set_tacho_speed_sp(motor[R], -max_speed/2);
+            multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
+			//Back up for 1 second
+            Sleep(1000);
+			//Order the motors to stop
+            multi_set_tacho_command_inx(motor, TACHO_STOP);
+			//randomly turn from 2 - 6 wheel rotations
+            turn_vehicle(max_speed/2, rand()%4 + 2);
+		
+		//If we left the loop we are in range we see black
+		//At this point we go full speed for a short while to push others off and stop
+        } else {
+			//Set full speed and drive forward
+            set_tacho_speed_sp(motor[L], max_speed);
+            set_tacho_speed_sp(motor[R], max_speed);
+            multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
+			//Forward for 200ms
+            Sleep(200);
+			//Order the motors to stop
+            multi_set_tacho_command_inx(motor, TACHO_STOP);
+        }
+		
+		//If it is the condition where we currently see black the car will stop
+		//If black is no longer detected we will begin to search again
         while(withinColRange(0, MAX_BLACK_RANGE)){
             multi_set_tacho_command_inx(motor, TACHO_STOP);
             get_sensor_value(0, sn_color, &color_value);
         }
+		
+		//Reset button flag
         is_button_pressed = 0;
     }
+	
+	//If we leave the while loop the vehicle will cease moving
     multi_set_tacho_command_inx(motor, TACHO_STOP);
     return(0);
 }
